d4c0da68620bcbd9e4cfc22eb2bf11c6c6165f08
biano <gufaduarte1997@gmail.com>
2024-07-24 17:46:51 -0300

    Projetos de aula 24-07

---
+++ Novos Programa/24-07/entrada/main.py
@@ -0,0 +1,5 @@
+# entrada de dados
+nome = input("Informe seu nome: ")
+
+# saída de dados
+print ("O nome do usuário é " + nome + ".")
---
+++ Novos Programa/24-07/variaveis/main.py
@@ -0,0 +1,10 @@
+# declaração de variaveis 
+nome: str = "Gustavo"
+idade: int = 27
+altura: float = 1.72
+programador: bool = True
+
+# saída de dados 
+print("Nome do usuário: " + nome + ".")
+print("Idade: " + str(idade) + ".")
+print("Altura: " + str(altura) + ".")
