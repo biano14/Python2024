2ee431c846d28366daea152f8743a332793e5ecf
biano <gufaduarte1997@gmail.com>
2024-07-24 17:52:20 -0300

    Add some changes

--- Novos Programa/24-07/entrada/main.py
+++ Novos Programa/24-07/entrada/main.py
@@ -1,5 +1,7 @@
 # entrada de dados
 nome = input("Informe seu nome: ")
+peso = input("Informe seu peso em kg: ")
 
 # saída de dados
 print ("O nome do usuário é " + nome + ".")
+print ("Peso: " + str(peso) + ".")
