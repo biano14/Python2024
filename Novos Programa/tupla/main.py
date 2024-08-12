768a31d07b12e0a89637906b09fcde775ceead48
biano14 <gufaduarte1997@gmai.com>
2024-08-12 14:55:23 -0300

    tupla

---
+++ Novos Programa/tupla/main.py
@@ -0,0 +1,14 @@
+#tupla
+paises = ('Brasil', 'Paraguai', 'Romênia', 'França', 'Espanha', 'Portugal', 'Argentina', 'China', 'Japão','Tailândia', 'Itália')
+
+#exibe a tupla
+for pais in paises:
+    print(pais)
+
+    print('\n')
+
+    # exibe o tipo de coleção
+    print(type(paises))
+
+    paises_lista = sorted(paises)
+    
