f4aace897b32a10b8012b213d73d62ee5b66e7d3
biano <gufaduarte1997@gmail.com>
2024-09-24 14:07:43 -0300

    re-upload "tradutor"

---
+++ Novos Programa/tradutor/main.py
@@ -0,0 +1,15 @@
+from deep_translator import GoogleTranslator
+
+tradutor = GoogleTranslator(source='auto', target='pt')
+while True:
+    try:
+        texto = input('Informe o texto a ser traduzido: ')
+        texto_traduzido = tradutor.translate(texto)
+        print( f'{texto_traduzido}\n')
+        repetir = input('Deseja traduzir outro texto (s/n): ').lower()
+        if repetir == 's':
+            continue
+        else:
+            break 
+    except:
+        print('Não foi possível traduzir o texto inserido') 
